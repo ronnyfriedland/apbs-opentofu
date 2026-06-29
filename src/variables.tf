5d9012b93bc1e73cb912b2192666b18302b031d6
kerstinli <101319871+kerstinli@users.noreply.github.com>
2026-06-29 15:12:21 +0200

    Update variables.tf

--- src/variables.tf
+++ src/variables.tf
@@ -2,3 +2,13 @@ variable "opensearch_password" {
   type      = string
   sensitive = true
 }
+
+variable "ssh_host" {
+  type        = string
+  description = "Zielhost für SSH-Verbindung zum Docker-Daemon"
+}
+
+variable "ssh_user" {
+  type        = string
+  description = "SSH-User auf dem Zielhost (muss in der docker-Gruppe sein)"
+}
